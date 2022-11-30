b810532b59bcbb2da109b49418c3d7f56f785b86
Andrey Isaev <an.isaev7@gmail.com>
2022-11-30 22:01:14 +0300

    add models

--- project/models.py
+++ project/models.py
@@ -1,9 +1,38 @@
-from sqlalchemy import Column, String
-
-from project.setup.db import models
+from project.setup.db import models, db
+from marshmallow import Schema, fields
 
 
 class Genre(models.Base):
     __tablename__ = 'genres'
 
-    name = Column(String(100), unique=True, nullable=False)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+
+
+class Director(models.Base):
+    __tablename__ = "directors"
+
+    name = db.Column(db.String(100), unique=True, nullable=False)
+
+
+class Movie(models.Base):
+    __tablename__ = "movies"
+
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    trailer = db.Column(db.String(200), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    rating  = db.Column(db.Float, nullable=False)
+    genre_id = db.Column(db.Integer,db.ForeignKey("genre.id"),nullable=False)
+    genre = db.relationship("Genre")
+    director_id = db.Column(db.Integer, db.ForeignKey("director.id"), nullable=False)
+    director = db.relationship("Director")
+
+class User(models.Base):
+    __tablename__ = "users"
+
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    surname = db.Column(db.String(100), unique=True, nullable=False)
+    favorite_genre = db.Column(db.Integer)
+
